234280e51f022cde87c8a0015073ecffc5a36cb5
smdeep <sudeep.mukherjee@gmail.com>
2025-01-26 23:57:11 +0530

    Changed header in init.lua, added markdown plugins and added keymap to insert date

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -5,3 +5,6 @@ local keymap = vim.keymap -- for conciseness
 
 -- use jk to exit insert mode
 keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode with jk' })
+
+-- Sunday 26 January 2025 11:55:11 PM Added keymap to insert current time
+keymap.set('n', '<leader>dt', "<cmd>pu=strftime('%c')<CR>", { desc = 'Insert current time in normal mode' })
