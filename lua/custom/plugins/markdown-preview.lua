2a9794dfdb749e039cfdaa8b771e55db1cceecb9
smdeep <sudeep.mukherjee@gmail.com>
2025-01-29 23:56:43 +0530

    Copied render-markdown config from LazyVim. Works now!

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -1,19 +1,19 @@
 return {
-  {
-    'iamcco/markdown-preview.nvim',
-    keys = {
-      {
-        '<leader>mp',
-        ft = 'markdown',
-        '<cmd>MarkdownPreviewToggle<cr>',
-        desc = 'Markdown Preview',
-      },
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  build = function()
+    require('lazy').load { plugins = { 'markdown-preview.nvim' } }
+    vim.fn['mkdp#util#install']()
+  end,
+  keys = {
+    {
+      '<leader>cp',
+      ft = 'markdown',
+      '<cmd>MarkdownPreviewToggle<cr>',
+      desc = 'Markdown Preview',
     },
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd app && yarn install',
-    init = function()
-      vim.g.mkdp_filetypes = { 'markdown' }
-    end,
-    ft = { 'markdown' },
   },
+  config = function()
+    vim.cmd [[do FileType]]
+  end,
 }
